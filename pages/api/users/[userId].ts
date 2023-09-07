38d787b371a26bf2c5f2ba618ab907ed1403aecd
gogoyooni <taeyoondev@gmail.com>
2023-09-07 22:04:55 +0900

    fixed a typo & added userHero & userBio

--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -25,6 +25,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         },
       },
     });
+
     return res.status(200).json({ ...existingUser, followersCount });
   } catch (error) {
     console.log(error);
