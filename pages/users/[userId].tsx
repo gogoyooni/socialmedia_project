c4cd5610eea28ea8f2f0287b1d5bd92ecd928cf7
gogoyooni <taeyoondev@gmail.com>
2023-09-15 11:03:20 +0900

    added a function of showing posts of users and a particular user

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -4,6 +4,7 @@ import { ClipLoader } from "react-spinners";
 import Header from "@/components/Header";
 import UserHero from "@/components/users/UserHero";
 import UserBio from "@/components/users/UserBio";
+import PostFeed from "@/components/posts/PostFeed";
 
 import useUser from "@/hooks/useUser";
 
@@ -26,6 +27,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };
